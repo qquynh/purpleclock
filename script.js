1b0a1a9c2671fb553500b886b5d9d6ae1f51e73d
qquynh <85900290+qquynh@users.noreply.github.com>
2023-11-01 23:49:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
-function updateClock() {
+function updateTime() {
     const now = new Date();
     let hours = now.getHours();
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const ampm = hours >= 12 ? 'PM' : 'AM';
+    let minutes = now.getMinutes();
+    let ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
-    const timeString = `${hours}:${minutes} ${ampm}`;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
 
-    const day = now.toLocaleDateString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-    const greeting = getGreeting(now.getHours());
-    
-    document.getElementById('greeting').innerText = greeting;
-    document.getElementById('clock').innerText = timeString;
-    document.getElementById('date').innerText = day;
+    document.querySelector('.clock').innerText = hours + ':' + minutes + ' ' + ampm;
+    // Removed the year from the date display
+    document.querySelector('.date').innerText = `${days[now.getDay()]}, ${months[now.getMonth()]} ${now.getDate()}`;
 }
 
-function getGreeting(hour) {
-    if (hour < 12) {
-        return "Good Morning";
-    } else if (hour < 18) {
-        return "Good Afternoon";
-    } else {
-        return "Good Evening";
-    }
-}
-
-setInterval(updateClock, 1000);
-updateClock();
+setInterval(updateTime, 1000);
+updateTime();
